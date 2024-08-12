332142623d941bb1735ecaf67f4867e963fc7b90
jameswhatcott <james@whatcott.com>
2024-08-12 17:46:04 -0500

    shifted pages to home-routes

--- server.js
+++ server.js
@@ -15,59 +15,5 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
-app.get('/', async (req, res) => {
-  try {
-    res.render('homepage');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
-
-app.get('/checkout', async (req, res) => {
-  try {
-    res.render('checkout');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
-
-app.get('/success', async (req, res) => {
-  try {
-    res.render('success');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-app.get('/cancel', async (req, res) => {
-  try {
-    res.render('cancel');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success`,
-    cancel_url: `${YOUR_DOMAIN}/cancel`,
-  });
-
-  res.redirect(303, session.url);
-});
 
 app.listen(4242, () => console.log('Running on port 4242'));
