36a0bca91cf56caed90871a01c83badcdf1c7d55
jameswhatcott <james@whatcott.com>
2024-08-15 15:27:47 -0400

    fixed the dropdown bar by adding the seeds

--- models/Cart.js
+++ models/Cart.js
@@ -4,23 +4,35 @@ const sequelize = require('../config/connection');
 class Cart extends Model{}
 
 Cart.init(
-            primaryKey: true
-        },
-       user_id:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true
-        },
-       quantity:{
-=======
-            references: {
-              model: 'user',
-              key: 'id'
-            },
-            onDelete: 'CASCADE',
-            onUpdate: 'CASCADE'
-          },
-          game_id: {
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'game',
+        key: 'id',
+      },
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  },
     {
         sequelize,
         timestamps: false,
