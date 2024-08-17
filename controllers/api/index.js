f5385fcb14aca541a8e1cf5d6e03c35dfb47ea11
BrianMN11 <nyamohangabrian@gmail.com>
2024-08-17 08:22:40 -0400

    updated index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const cartRoute = require('./cartRoute');
 const consoleRoutes = require('./consoleRoutes');
-const gameRoutes = require('./consoleRoutes');
+const gameRoutes = require('./gameRoutes');
 const gamesConsoleRoutes = require('./gamesConsoleRoutes');
 const userRoute = require('./userRoute');
-const sucessRoute = require('./sucessRoute');
 
 
 router.use('/users', userRoute);
@@ -12,7 +11,6 @@ router.use('/cart', cartRoute);
 router.use('/consoles', consoleRoutes);
 router.use('/games', gameRoutes);
 router.use('/gamesConsole', gamesConsoleRoutes);
-router.use('/success', sucessRoute);
 
 
 module.exports = router
