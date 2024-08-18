46c033e070bb87c1cb5259626a6eb8355747b097
BrianMN11 <nyamohangabrian@gmail.com>
2024-08-18 16:46:01 -0400

    updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 
 module.exports = {
-  content: [],
+  content: ["./views/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      colors: {
+        'winter' : '#09203F',
+        'spring' : '#537895',
+        'summer' : '#dddddd',
+      }
+    },
   },
   plugins: [],
 }
